f8627a8658bc228b73f7ddeb969a76b28c0e69c3
Vilius Roškus <vro@danskebank.lt>
2022-10-27 14:49:52 +0300

    Cleanup

--- packages/web/cypress/support/commands.js
+++ packages/web/cypress/support/commands.js
@@ -23,11 +23,3 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-Cypress.Commands.add('login', () => {
-  cy.setCookie('accessToken', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3YjE0NGM3LWI4YjktNGNkNy1hYjM4LWViYmRmYjQ3ZWJjYyIsInNjb3BlIjpbXSwiaWF0IjoxNTY5ODQ1MTMzLCJleHAiOjI1MjQ2MDgwMDB9.K3zey0-aLyJMZEobry-mgf3R9BqEEJkVJ_KqyKjPQ_I');
-});
-
-Cypress.Commands.add('logout', () => {
-  cy.clearCookie('accessToken');
-});
